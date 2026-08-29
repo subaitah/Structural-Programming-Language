6c50fbc3a43950cccb7c7993b808b372241d8b44
Subaitah Chowdhury <subaitahchy16@gmail.com>
2026-08-29 13:37:11 +0600

    Adding W3School C problem

---
+++ W3School_Receipt.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    float price[100];
+    char item[100][100];
+    int n;
+    float total = 0;
+    printf("Enter number of items: \n");
+    scanf("%d", &n);
+    for(int j = 0; j<n; j++){
+        printf("Enter name of item %d: \n", j+1);
+        scanf("%s", item[j]);
+        printf("Enter the price of %s: \n", item[j]);
+        scanf("%f", &price[j]);
+        total += price[j];       
+    }
+    for(int i=0; i<n; i++){
+        printf("The price of %s is %.2f\n", item[i], price[i]);
+    }
+    printf("Total items: %d\n", n);
+    printf("Total price: %.2f\n", total);
+    int paid;
+    printf("Enter customer paid amount: \n");
+    scanf("%d", &paid);
+    printf("Cash return: %f", paid - total);
+    return 0;
+
+}
